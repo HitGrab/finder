24d15192aede939865d74ee6ea2ad74160a9d5b6
Andrew Stevenson <60524600+cardboardshark@users.noreply.github.com>
2025-05-10 14:59:10 -0400

    gitignore

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+export { useFinderFactory } from './use-finder-factory';
+export { useFinder } from './use-finder';
+export { FinderItems } from './finder-items';
+export { Finder } from './finder';
+export { FinderEmpty } from './finder-empty';
+export { FinderGroups } from './finder-groups';
+export { FinderLoading } from './finder-loading';
+export { FinderContent } from './finder-content';
