31658003357ca636e55747bdf43f9c9f1e8b11d4
Andrew Stevenson <60524600+cardboardshark@users.noreply.github.com>
2025-06-10 16:41:23 -0400

    Export NoMatches component (#19)

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export { Finder } from "./react/components/finder";
 export { FinderEmpty } from "./react/components/finder-empty";
 export { FinderGroups } from "./react/components/finder-groups";
 export { FinderLoading } from "./react/components/finder-loading";
+export { FinderNoMatches } from "./react/components/finder-no-matches";
 export { FinderContent } from "./react/components/finder-content";
 export { useFinderRef } from "./react/hooks/use-finder-ref";
 export { finderStringCompare, finderCharacterCompare, finderSequentialCharacterCompare } from "./core/utils/string-compare-utils";
