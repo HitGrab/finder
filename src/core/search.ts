9631a805e059031e24923f5c23cf82e2612d887a
Andrew Stevenson <60524600+cardboardshark@users.noreply.github.com>
2025-10-31 17:20:34 -0400

    Moving search algorithm out of mixin

--- src/core/search.ts
+++ src/core/search.ts
@@ -1,18 +1,11 @@
-import { orderBy } from "lodash";
 import { isSearchRule } from "./utils/rule-utils";
 import { ERRORS, EVENT_SOURCE, EVENTS } from "./core-constants";
 import { FinderError } from "./finder-error";
-import { calculateSearchScore } from "./search/search-score";
-import { transformStringForComparison } from "./search/search-string-transform";
-import { SearchScore, MixinInjectedDependencies, SerializedSearchMixin } from "./types/core-types";
-import { calculateCharacterMatchIndexes } from "./search/calculate-character-match-indexes";
+import { MixinInjectedDependencies, SerializedSearchMixin } from "./types/core-types";
+import { defaultSearchAndSortAlgorithm } from "./search/default-search-and-sort-algorithm";
 
 interface InitialValues {
     initialSearchTerm: string | undefined;
-}
-interface SearchScoreItem<FItem> {
-    item: FItem;
-    score: SearchScore;
 }
 
 class SearchMixin<FItem> {
@@ -97,67 +90,7 @@ class SearchMixin<FItem> {
             return items;
         }
 
-        const matches = items.reduce<SearchScoreItem<FItem>[]>((acc, item) => {
-            if (options.rule?.searchFn === undefined) {
-                return acc;
-            }
-
-            // Retrieve this item's array of haystack strings to compare the search needle against
-            const itemHaystackStringOrStrings = options.rule.searchFn(item, context);
-            const itemHaystacks = Array.isArray(itemHaystackStringOrStrings)
-                ? itemHaystackStringOrStrings.map(transformStringForComparison)
-                : [transformStringForComparison(itemHaystackStringOrStrings)];
-
-            // an item may have multiple matches if it has multiple haystack strings.
-            const itemHaystackScores = itemHaystacks.reduce<ReturnType<typeof calculateSearchScore>[]>((scores, haystack) => {
-                const indexes = calculateCharacterMatchIndexes(haystack, options.searchTerm);
-                if (indexes !== undefined) {
-                    scores.push(calculateSearchScore(indexes, haystack));
-                }
-                return scores;
-            }, []);
-
-            // determine which ( if any ) of the aliases have the best score.
-            if (itemHaystackScores.length > 0) {
-                const sortedItemHaystackScores = orderBy(itemHaystackScores, ["percentOfHaystackMatched", "longestSequentialSequence"], ["desc", "asc"]);
-                const bestScore = sortedItemHaystackScores.at(0);
-
-                if (bestScore) {
-                    acc.push({ item, score: bestScore });
-                }
-            }
-
-            return acc;
-        }, []);
-
-        const overallLongestSequentialSequence = matches.reduce((length, match) => {
-            if (match.score.longestSequentialSequence > length) {
-                length = match.score.longestSequentialSequence;
-            }
-            return length;
-        }, 0);
-
-        // sort all results by their search score.
-        const sortedMatches = orderBy(
-            matches,
-            [
-                (match) => {
-                    // A ratio of matched characters to haystack length. ( 0 - 100 )
-                    // With the needle 'ap', 'app' has a score of 66, while 'apple' is 40.
-                    const percentMatchScore = match.score.percentOfHaystackMatched * 100;
-
-                    // Rank all matches by their relative number of sequential matches. ( 0 - 100)
-                    // With the needle 'orange', 'orc angel' has a score of 80, while 'delicious oranges' is 100.
-                    const sequenceScore = (match.score.longestSequentialSequence / overallLongestSequentialSequence) * 100;
-
-                    // Equally weight both results for a score between 0 - 200.
-                    return percentMatchScore + sequenceScore;
-                },
-            ],
-            ["desc"],
-        );
-
-        return sortedMatches.map((match) => match.item);
+        return defaultSearchAndSortAlgorithm(options, items, context);
     }
 }
 
---
+++ src/core/search/default-search-and-sort-algorithm.ts
@@ -0,0 +1,74 @@
+import { orderBy } from "lodash";
+import { SearchScore, SerializedSearchMixin } from "../types/core-types";
+import { calculateCharacterMatchIndexes } from "./calculate-character-match-indexes";
+import { calculateSearchScore } from "./search-score";
+import { transformStringForComparison } from "./search-string-transform";
+
+interface SearchScoreItem<FItem> {
+    item: FItem;
+    score: SearchScore;
+}
+
+export function defaultSearchAndSortAlgorithm<FItem>(options: SerializedSearchMixin, items: FItem[], context: unknown) {
+    const matches = items.reduce<SearchScoreItem<FItem>[]>((acc, item) => {
+        if (options.rule?.searchFn === undefined) {
+            return acc;
+        }
+
+        // Retrieve this item's array of haystack strings to compare the search needle against
+        const itemHaystackStringOrStrings = options.rule.searchFn(item, context);
+        const itemHaystacks = Array.isArray(itemHaystackStringOrStrings)
+            ? itemHaystackStringOrStrings.map(transformStringForComparison)
+            : [transformStringForComparison(itemHaystackStringOrStrings)];
+
+        // an item may have multiple matches if it has multiple haystack strings.
+        const itemHaystackScores = itemHaystacks.reduce<ReturnType<typeof calculateSearchScore>[]>((scores, haystack) => {
+            const indexes = calculateCharacterMatchIndexes(haystack, options.searchTerm);
+            if (indexes !== undefined) {
+                scores.push(calculateSearchScore(indexes, haystack));
+            }
+            return scores;
+        }, []);
+
+        // determine which ( if any ) of the aliases have the best score.
+        if (itemHaystackScores.length > 0) {
+            const sortedItemHaystackScores = orderBy(itemHaystackScores, ["percentOfHaystackMatched", "longestSequentialSequence"], ["desc", "asc"]);
+            const bestScore = sortedItemHaystackScores.at(0);
+
+            if (bestScore) {
+                acc.push({ item, score: bestScore });
+            }
+        }
+
+        return acc;
+    }, []);
+
+    const overallLongestSequentialSequence = matches.reduce((length, match) => {
+        if (match.score.longestSequentialSequence > length) {
+            length = match.score.longestSequentialSequence;
+        }
+        return length;
+    }, 0);
+
+    // sort all results by their search score.
+    const sortedMatches = orderBy(
+        matches,
+        [
+            (match) => {
+                // A ratio of matched characters to haystack length. ( 0 - 100 )
+                // With the needle 'ap', 'app' has a score of 66, while 'apple' is 40.
+                const percentMatchScore = match.score.percentOfHaystackMatched * 100;
+
+                // Rank all matches by their relative number of sequential matches. ( 0 - 100)
+                // With the needle 'orange', 'orc angel' has a score of 80, while 'delicious oranges' is 100.
+                const sequenceScore = (match.score.longestSequentialSequence / overallLongestSequentialSequence) * 100;
+
+                // Equally weight both results for a score between 0 - 200.
+                return percentMatchScore + sequenceScore;
+            },
+        ],
+        ["desc"],
+    );
+
+    return sortedMatches.map((match) => match.item);
+}
