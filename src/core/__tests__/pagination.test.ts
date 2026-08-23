9cdfefbaa559590f9862d6cdd34467cc534e4c19
Andrew Stevenson <60524600+cardboardshark@users.noreply.github.com>
2026-08-22 23:48:58 -0400

    Fixing bug with pagination count,adding test for safety (#99)

--- src/core/__tests__/pagination.test.ts
+++ src/core/__tests__/pagination.test.ts
@@ -1,5 +1,6 @@
+import { range } from "lodash";
 import { FinderCore } from "../finder-core";
-import { sortByRule } from "../utils/rule-type-guards";
+import { filterRule, sortByRule } from "../utils/rule-type-guards";
 import { objectItems, banana, orange, apple } from "./test-constants";
 import { MockObjectItem } from "./test-types";
 
@@ -58,5 +59,19 @@ describe("Pagination", () => {
         finder.sortBy.toggleSortDirection();
         expect(finder.pagination.page).toEqual(1);
         expect(finder.matches.items).toEqual([apple]);
+    });
+
+    test("Counts are correct", () => {
+        const items = range(300);
+        const rule = filterRule({
+            id: "even_only",
+            filterFn: (item: number) => item % 2 === 0,
+            boolean: true,
+            defaultValue: true,
+        });
+        const finder = new FinderCore(items, { rules: [rule], page: 10, numItemsPerPage: 10 });
+        expect(finder.matches.items).toEqual([180, 182, 184, 186, 188, 190, 192, 194, 196, 198]);
+        expect(finder.matches.numMatchedItems).toBe(150);
+        expect(finder.matches.numTotalItems).toBe(300);
     });
 });
